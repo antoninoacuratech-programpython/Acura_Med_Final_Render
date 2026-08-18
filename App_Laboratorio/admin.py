63e3f3f76b4deadcd25921597673946ebc468402
antoninoacuratech-programpython <antoninoacuratech@gmail.com>
2026-08-18 11:07:49 -0300

    sistema final laboratorio

--- App_Laboratorio/admin.py
+++ App_Laboratorio/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .item_solicitacao_exame import TipoExame
+from .tipo_exame import TipoExame
 
 
 @admin.register(TipoExame)
 class TipoExameAdmin(admin.ModelAdmin):
-    list_display = ("codigo", "nome", "categoria", "tipo_amostra", "tempo_estimado_horas", "ativo")
-    list_filter = ("categoria", "tipo_amostra", "ativo")
-    search_fields = ("codigo", "nome")
+    list_display = ("codigo", "nome", "departamento", "tipo_amostra", "tempo_estimado", "ativo")
+    list_filter = ("departamento", "tipo_amostra", "metodo", "tipo_resultado", "ativo")
+    search_fields = ("codigo", "codigo_padronizado", "nome", "nome_tecnico")
     ordering = ("nome",)
     list_editable = ("ativo",)
     readonly_fields = ("criado_em", "atualizado_em")
